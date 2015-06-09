e8fa6bf8bd2037d1c26c7a41780aebcdfb9e2607
Ramiro Gómez <code@ramiro.org>
2015-06-09 16:01:46 +0200

    Append text image to the bottom rather than stacked above.

--- wim/wim.py
+++ wim/wim.py
@@ -5,13 +5,29 @@ import click
 from os.path import splitext
 from PIL import Image, ImageDraw, ImageFont
 
+MODE = 'RGBA'
+BLACK = (0, 0, 0, 255)
+WHITE = (255, 255, 255, 255)
+
 
 def add_text(img, font, fontsize, text):
     font = ImageFont.truetype(font, fontsize)
     text_width, text_height = font.getsize(text)
-    draw = ImageDraw.Draw(img, 'RGBA')
-    pos = ((img.size[0] - text_width) / 2, (img.size[1] - text_height))
-    draw.text(pos, text, (0, 0, 0), font=font)
+    text_img_height = int(text_height * 1.5)
+
+    base_img = Image.new(MODE, (img.size[0], text_img_height + img.size[1]), BLACK)
+
+    text_image = Image.new(MODE, (img.size[0], text_img_height), BLACK)
+    draw = ImageDraw.Draw(text_image, MODE)
+
+    # Center text on image.
+    pos = ((text_image.size[0] - text_width) / 2, (text_image.size[1] - text_height) / 2)
+    draw.text(pos, text, WHITE, font=font)
+
+    base_img.paste(img, (0, 0))
+    base_img.paste(text_image, (0, img.size[1]))
+
+    return base_img
 
 
 @click.command()
@@ -30,11 +46,11 @@ def main(inplace, font, fontsize, text, filename):
         parts = splitext(filename)
         outfile = parts[0] + '.wim' + parts[1]
 
-    if img.mode != 'RGBA':
-        img = img.convert('RGBA')
+    if img.mode != MODE:
+        img = img.convert(MODE)
 
     if text:
-        add_text(img, font, fontsize, text)
+        img = add_text(img, font, fontsize, text)
 
     img.save(outfile)
 
